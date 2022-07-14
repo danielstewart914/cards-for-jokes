6e755ec1de84117f0ee00ad25fe7cea158bb7a51
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-07-13 21:32:25 -0700

    Added modal functions and user input

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,17 @@ var bottomCardRowEl = $( '#bottomCardRow' );
 var deckOfCardApiRootUrl = 'https://deckofcardsapi.com/api/deck';
 var deckId = JSON.parse( localStorage.getItem( 'deck_id' ) );
 
+// user settings
+var userName = JSON.parse( localStorage.getItem( 'user_name' ) );
+var themeIndex = JSON.parse( localStorage.getItem( 'deck_theme' ) );
+
+const themes = [ 'https://deckofcardsapi.com/static/img/back.png', './assets/images/batman-card-theme.jpeg' ];
+
+// index.html elements
+var highCardGameEl = $( '#highCardGame' );
+var userModal = $( '#user-modal' );
+var playGameElButton = $( '#play-game' );
+var usernameEntryEl = $( '#username-entry' );
 
 // initialize data
 function initialize () {
@@ -120,3 +131,34 @@ function renderBottomRow () {
 }
 
 initialize();
+
+highCardGameEl.on( 'click', function() {
+
+  if( userName ) location.href = 'gamepage.html'
+  else userModal.modal( 'open' );
+
+} );
+
+userModal.on( 'click', 'img', function ( event ) {
+
+  var themeSelection = $( event.target );
+  var otherThemes = userModal.children().children().children( 'img' ).not( themeSelection );
+  console.log( otherThemes );
+
+  themeSelection.addClass( 'selected-theme' );
+  otherThemes.removeClass( 'selected-theme' );
+
+  themeIndex = parseInt( themeSelection.data( 'theme' ) );
+
+} );
+
+playGameElButton.on( 'click', function() {
+
+  if ( !usernameEntryEl.val() ) return;
+
+  userName = usernameEntryEl.val();
+
+  if ( !themeIndex ) themeIndex = 0;
+
+
+} );
