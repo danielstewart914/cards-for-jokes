82ce21a75ee207e0bf8eb0ca04d6544d93dd36d1
Divyapriyadharshini Babu <divyapriyadharshinibabu@gmail.com>
2022-07-14 17:24:29 -0700

    Added separate js file for game page

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,6 @@ var nameEntryErrorEl = $( '#name-entry-error' );
 
 var jokeAPIUrl ='https://v2.jokeapi.dev/joke/Programming';
 
-//Card retrieval
-var centerThemeCard = $('#center-theme');
-var userCardEl = $('#user-card');
-var computerCardEl = $('#computer-card');
-
 // initialize modals on all pages where main.js is loaded
 $(document).ready(function(){
   $('.modal').modal();
@@ -31,7 +26,6 @@ $(document).ready(function(){
 $(document).ready(function () {
   $(".sidenav").sidenav();
 });
-
 
 // initialize data
 function initialize () {
@@ -52,56 +46,6 @@ function initialize () {
 
     renderBottomRow();
 
-  }
-
-}
-
-centerThemeCard.on('click', function(event) {
-  event.preventDefault();
-  console.log('here');
-  getNewDeck(1).then( function(data) {
-    deckId = data.deck_id;
-    shuffleDeck(deckId).then (function () {
-      drawCard(2).then(function(data) {
-        console.log(data);
-        userCardEl.css('background-image', 'url('+ data.cards[0].image+')');
-        computerCardEl.css('background-image', 'url('+ data.cards[1].image+')');
-
-        // Determine who is the winner
-        determineWinner(data.cards[0].value,data.cards[1].value);
-      });
-    });
-  });
-});
-
-function determineWinner(user_val, comp_val) {
-
-
-  var cards_value = {
-    'KING' : 13,
-    'QUEEN' : 12,
-    'JACK' : 11,
-    'ACE' : 0
-  };
-
-  if (cards_value[user_val] !== undefined){
-    user_val = cards_value[user_val];
-  }
-
-  if (cards_value[comp_val] !== undefined){
-    comp_val = cards_value[comp_val];
-  }
-
-  user_val = parseInt(user_val);
-  comp_val = parseInt(comp_val);
-
-  if(user_val == comp_val){
-    console.log("Nobody wins");
-  } else if(user_val > comp_val ) {
-    console.log("Userwins");
-    getJoke();
-  } else {
-    console.log("Computer wins");
   }
 
 }
