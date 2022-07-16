8f6d23a65a617003a34741580c95fbc01ae14521
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-07-16 11:41:53 -0700

    A little polish on the joke modal

--- assets/js/gamepage.js
+++ assets/js/gamepage.js
@@ -2,6 +2,7 @@ var centerThemeCard = $('#center-theme');
 var userCardEl = $('#user-card');
 var computerCardEl = $('#computer-card');
 var jokeModal = $( '#jokeModal' );
+var jokeBoxEl = $( '#jokeBox' )
 var score = 0;
 
   function determineWinner(user_val, comp_val) {
@@ -28,15 +29,16 @@ var score = 0;
       console.log("Nobody Wins");
     } else if(user_val > comp_val ) {
       console.log("User Wins");
+      jokeBoxEl.html('');
       jokeModal.modal( 'open' );
       score +=10;
       getJoke().then( function(data) {
         if (data.type = 'single' && data.joke) {
           console.log(data.joke);
-          document.getElementById("jokeBox").innerHTML = data.joke;
+          jokeBoxEl.html( data.joke );
         } else {
           console.log(data.setup + data.delivery)
-          document.getElementById("jokeBox").innerHTML = `<p>${data.setup}</p><p>${data.delivery}</p>`;
+          jokeBoxEl.html( `<p>${data.setup}</p><p>${data.delivery}</p>` );
         }
       })
     } else {
@@ -70,43 +72,3 @@ centerThemeCard.on('click', function(event) {
 
   });
 });
-
-  /*function determineWinner(user_val, comp_val) {
-
-    var cards_value = {
-      'KING' : 13,
-      'QUEEN' : 12,
-      'JACK' : 11,
-      'ACE' : 0
-    };
-  
-    if (cards_value[user_val] !== undefined){
-      user_val = cards_value[user_val];
-    }
-  
-    if (cards_value[comp_val] !== undefined){
-      comp_val = cards_value[comp_val];
-    }
-  
-    user_val = parseInt(user_val);
-    comp_val = parseInt(comp_val);
-  
-    if(user_val == comp_val){
-      console.log("Nobody wins");
-    } else if(user_val > comp_val ) {
-      console.log("Userwins");
-      score += 10;
-      getJoke().then( function(data) {
-        // This does not work but why?
-        if (data.type = 'single') {
-          console.log(data.joke);
-          document.getElementById("jokeBox").innerHTML = (data.joke)
-        } else {
-          console.log(data.setup + data.delivery)
-          document.getElementById("jokeBox").innerHTML = (data.setup + data.delivery);
-        }
-      });
-    } else {
-      console.log("Computer wins");
-    }
-} */
