9db8c35a5d9f3ccf1da730cc3f283c309cedb812
mrcaseyrobison <104058949+mrcaseyrobison@users.noreply.github.com>
2022-07-16 09:33:23 -0700

    fixed merge conflicts

--- assets/js/gamepage.js
+++ assets/js/gamepage.js
@@ -76,12 +76,7 @@ var score = 0;
 centerThemeCard.on('click', function(event) {
   event.preventDefault();
   drawCard(2).then(function(data) {
-<<<<<<< HEAD
-    userCardEl.css('background-image', 'url('+ data.cards[0].
-    image+')');
-=======
     userCardEl.css('background-image', 'url('+ data.cards[0].image+')');
->>>>>>> main
     computerCardEl.css('background-image', 'url('+ data.cards[1].image+')');
   
     // Determine who is the winner
